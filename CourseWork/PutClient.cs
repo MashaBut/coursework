4543a258ac1128de8d3dadeaa61c0cdb411e3efe
MashaBut <42973147+MashaBut@users.noreply.github.com>
2019-01-24 14:33:59 +0200

    debug Sing in

--- CourseWork/PutClient.cs
+++ CourseWork/PutClient.cs
@@ -11,12 +11,16 @@ namespace CourseWork
         {
             string connStr = "server=localhost;user=root;database=coursework;password=mashutkabut99@gmail.com;";
             MySqlConnection conn = new MySqlConnection(connStr);
-            conn.Open();
-            sqlClient = $"select Password from coursework.client where Login='{Name}'";  
-            MySqlCommand command = new MySqlCommand(sqlClient, conn);
-            try { rez = command.ExecuteScalar().ToString();}
+            try
+            {
+                conn.Open();
+                sqlClient = $"select Password from coursework.client where Login='{Name}'";
+                MySqlCommand command = new MySqlCommand(sqlClient, conn);
+                try { rez = command.ExecuteScalar().ToString(); }
+                catch { }
+                conn.Close();
+            }
             catch { }
-            conn.Close();
         }
     }
 }
